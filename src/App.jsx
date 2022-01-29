562e0db64dfcc309b427a63326b056d628b09157
Alison Santos <alisonsantoso22@gmail.com>
2022-01-29 08:30:38 -0300

    Update font family

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 import Footer from './components/Footer';
 
+import './styles/global.scss';
+
 const App = () => {
   return (
     <>
