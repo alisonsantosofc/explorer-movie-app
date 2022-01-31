b4ef7ff5d684e62640f7124adf4a160bb8ea0cb6
Alison Santos <alisonsantoso22@gmail.com>
2022-01-31 14:26:48 -0300

    Create MovieModal component and update styles

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,36 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import Modal from 'react-modal';
-import { FaVideoSlash, FaChevronDown } from 'react-icons/fa';
 
-import { getMovieInfo, getMovieVideo } from '../../services/apiTmdb';
+import MovieModal from '../MovieModal';
 
 import './styles.scss';
 
 Modal.setAppElement('#root');
 
 const Movie = ({ info }) => {
-  const { id, poster_path } = info;
+  const { id, title, poster_path } = info;
 
   const [modalIsOpen, setIsOpen] = useState(false);
-  const [movieData, setMovieData] = useState({});
-  const [movieVideo, setMovieVideo] = useState({});
-
-  useEffect(() => {
-    const loadMovieInfo = async () => {
-      const data = await getMovieInfo(id);
-
-      setMovieData(data);
-    };
-
-    const loadMovieVideo = async () => {
-      const video = await getMovieVideo(id);
-
-      setMovieVideo(video);
-    };
-
-    loadMovieInfo();
-    loadMovieVideo();
-  }, []);
 
   async function handleOpenModal() {
     setIsOpen(true);
@@ -39,38 +19,6 @@ const Movie = ({ info }) => {
   function handleCloseModal() {
     setIsOpen(false);
   }
-
-  const {
-    title,
-    tagline,
-    genres,
-    runtime,
-    overview,
-    backdrop_path,
-    release_date,
-  } = movieData;
-
-  const listVideo = [];
-  const { results } = movieVideo;
-
-  for (let i in results) {
-    listVideo.push(results[i].key);
-  }
-
-  const movieGeners = [];
-
-  for (let i in genres) {
-    movieGeners.push(genres[i].name);
-  }
-
-  const optionsDate = {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
-  };
-
-  const date = new Date(release_date);
-  const releaseDate = date.toLocaleDateString('pt-br', optionsDate);
 
   return (
     <>
@@ -84,57 +32,11 @@ const Movie = ({ info }) => {
           <h4>{title}</h4>
         </div>
       </button>
-      <Modal
-        className="movie-modal"
-        isOpen={modalIsOpen}
-        onRequestClose={handleCloseModal}
-        contentLabel="Modal Movie"
-        style={{ overlay: { backgroundColor: 'transparent' } }}
-      >
-        <img
-          src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
-          alt="Movie Background"
-        />
-
-        <div className="modal-container">
-          <div className="movie-info">
-            <div className="movie-content">
-              <div className="title-container">
-                <h2>{title}</h2>
-                <button type="button" onClick={handleCloseModal}>
-                  <FaChevronDown />
-                </button>
-              </div>
-
-              <p>{tagline}</p>
-
-              <dl>
-                <dt className="yellow">Estréia:</dt>
-                <dd>{releaseDate}</dd>
-                <dt>Gênero:</dt>
-                <dd>{movieGeners.join(', ')}</dd>
-                <dt>Duração:</dt>
-                <dd>{runtime}min</dd>
-              </dl>
-
-              <p className="movie-overview">{overview}</p>
-            </div>
-          </div>
-          {listVideo[0] ? (
-            <iframe
-              className="movie-video"
-              src={`https://www.youtube.com/embed/${listVideo[0]}`}
-              frameBorder={0}
-              allowFullscreen="true"
-            ></iframe>
-          ) : (
-            <div className="movie-video">
-              <FaVideoSlash className="video-notfound" />
-              <h3>Não encontramos nenhum video :(</h3>
-            </div>
-          )}
-        </div>
-      </Modal>
+      <MovieModal
+        movieId={id}
+        isModalOpen={modalIsOpen}
+        onModalClose={handleCloseModal}
+      />
     </>
   );
 };
---
+++ src/components/MovieModal/index.jsx
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from 'react';
+import Modal from 'react-modal';
+import { FaVideoSlash, FaChevronDown } from 'react-icons/fa';
+
+import { getMovieInfo, getMovieVideo } from '../../services/apiTmdb';
+
+import './styles.scss';
+
+const MovieModal = (props) => {
+  const { movieId, isModalOpen, onModalClose } = props;
+
+  const [movieData, setMovieData] = useState({});
+  const [movieVideo, setMovieVideo] = useState({});
+
+  useEffect(() => {
+    const loadMovieInfo = async () => {
+      const data = await getMovieInfo(movieId);
+
+      setMovieData(data);
+    };
+
+    const loadMovieVideo = async () => {
+      const video = await getMovieVideo(movieId);
+
+      setMovieVideo(video);
+    };
+
+    loadMovieInfo();
+    loadMovieVideo();
+    console.log(movieId);
+  }, [movieId]);
+
+  const {
+    title,
+    tagline,
+    genres,
+    runtime,
+    overview,
+    backdrop_path,
+    release_date,
+  } = movieData;
+
+  const listVideo = [];
+  const { results } = movieVideo;
+
+  for (let i in results) {
+    listVideo.push(results[i].key);
+  }
+
+  const movieGeners = [];
+
+  for (let i in genres) {
+    movieGeners.push(genres[i].name);
+  }
+
+  const optionsDate = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  };
+
+  const date = new Date(release_date);
+  const releaseDate = date.toLocaleDateString('pt-br', optionsDate);
+
+  return (
+    <Modal
+      className="movie-modal"
+      isOpen={isModalOpen}
+      onRequestClose={onModalClose}
+      contentLabel="Modal Movie"
+      style={{ overlay: { backgroundColor: 'transparent' } }}
+    >
+      <img
+        src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
+        alt="Movie Background"
+      />
+
+      <div className="modal-container">
+        <div className="movie-info">
+          <div className="movie-content">
+            <div className="title-container">
+              <h2>{title}</h2>
+              <button type="button" onClick={onModalClose}>
+                <FaChevronDown />
+              </button>
+            </div>
+
+            <p>{tagline}</p>
+
+            <dl>
+              <dt className="yellow">Estréia:</dt>
+              <dd>{releaseDate}</dd>
+              <dt>Gênero:</dt>
+              <dd>{movieGeners.join(', ')}</dd>
+              <dt>Duração:</dt>
+              <dd>{runtime}min</dd>
+            </dl>
+
+            <p className="movie-overview">{overview}</p>
+          </div>
+        </div>
+        {listVideo[0] ? (
+          <iframe
+            className="movie-video"
+            src={`https://www.youtube.com/embed/${listVideo[0]}`}
+            frameBorder={0}
+            allowFullscreen="true"
+          ></iframe>
+        ) : (
+          <div className="movie-video">
+            <FaVideoSlash className="video-notfound" />
+            <h3>Não encontramos nenhum video :(</h3>
+          </div>
+        )}
+      </div>
+    </Modal>
+  );
+};
+
+export default MovieModal;
